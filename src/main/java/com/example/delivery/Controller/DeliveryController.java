a50239446392cc584765d47bfd66f5c5181b1977
MaryShobisha <142608143+MaryShobisha@users.noreply.github.com>
2023-11-05 08:30:34 +0530

    Create DeliveryController.java
    Delivery Controller Committed

---
+++ src/main/java/com/example/delivery/Controller/DeliveryController.java
@@ -0,0 +1,35 @@
+package com.example.delivery.Controller;
+
+import com.example.delivery.entity.DeliveryStatus;
+import com.example.delivery.service.DeliveryService;
+import com.example.delivery.entity.Delivery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/delivery")
+public class DeliveryController {
+
+    @Autowired
+    private DeliveryService deliveryService;
+
+    @GetMapping("/allDelivers")
+    public List<Delivery> allDelivers(){
+        return deliveryService.allDelivers();
+    }
+
+    @PostMapping("/createDeliver")
+    public Delivery createDelivery(@RequestBody Delivery delivery){
+
+        return deliveryService.createDelivery(delivery);
+    }
+
+    @PutMapping("/updateStatus/{deliveryId}")
+    public String updateDeliveryStatus(@PathVariable Long deliveryId,@RequestParam DeliveryStatus deliveryStatus){
+        deliveryService.updateDeliveryStatus(deliveryId,deliveryStatus);
+        return "delivery status updated";
+
+    }
+
+}
