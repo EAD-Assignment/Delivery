e92cbcfe97eef1e82c7010db592bfdd8bc91ca88
MaryShobisha <142608143+MaryShobisha@users.noreply.github.com>
2023-11-09 22:04:07 +0530

    Update DeliveryController.java

--- src/main/java/com/example/delivery/Controller/DeliveryController.java
+++ src/main/java/com/example/delivery/Controller/DeliveryController.java
@@ -1,27 +1,39 @@
 package com.example.delivery.Controller;
 
 import com.example.delivery.entity.DeliveryStatus;
+import com.example.delivery.entity.EmailMessage;
 import com.example.delivery.service.DeliveryService;
 import com.example.delivery.entity.Delivery;
+import com.example.delivery.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/delivery")
 public class DeliveryController {
 
     @Autowired
+    private final EmailSenderService emailSenderService;
+    @Autowired
     private DeliveryService deliveryService;
 
-    @GetMapping("/allDelivers")
-    public List<Delivery> allDelivers(){
-        return deliveryService.allDelivers();
+    public DeliveryController(EmailSenderService emailSenderService) {
+
+        this.emailSenderService = emailSenderService;
     }
 
-    @PostMapping("/createDeliver")
-    public Delivery createDelivery(@RequestBody Delivery delivery){
+    @GetMapping("/getDelivery/{deliveryId}")
+    public ResponseEntity<Delivery> getDeliveryById(@PathVariable Long deliveryId) {
+        Optional<Delivery> optionalDelivery = deliveryService.getDeliveryById(deliveryId);
+        return optionalDelivery.map(delivery -> ResponseEntity.ok(delivery))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
+    @PostMapping("/createDelivery")
+    public Delivery createDelivery(@RequestBody Delivery delivery){
         return deliveryService.createDelivery(delivery);
     }
 
@@ -36,5 +48,6 @@ public class DeliveryController {
         this.emailSenderService.sendEmail(emailMessage.getTo(),emailMessage.getSubject(),emailMessage.getMessage());
         return "Email has been send successfully to customers.";
     }
-
+    
 }
+
