a468b33b83bb697b08ff8bd95b1d3ad12bde486f
MaryShobisha <142608143+MaryShobisha@users.noreply.github.com>
2023-11-09 22:07:25 +0530

    Update DeliveryService.java

--- src/main/java/com/example/delivery/service/DeliveryService.java
+++ src/main/java/com/example/delivery/service/DeliveryService.java
@@ -5,26 +5,30 @@ import com.example.delivery.entity.DeliveryStatus;
 import com.example.delivery.repo.DeliveryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DeliveryService {
-
-
     @Autowired
     private DeliveryRepo deliveryRepo;
-    public List<Delivery> allDelivers() {
+    
+        public Delivery createDelivery(Delivery delivery) {
 
-        return deliveryRepo.findAll();
-    }
-
-    public Delivery createDelivery(Delivery delivery) {
         return deliveryRepo.save(delivery);
+        }
+
+        public void updateDeliveryStatus(Long deliveryId, DeliveryStatus deliveryStatus) {
+        Delivery updateDeliveryStatus = deliveryRepo.findById(deliveryId).orElse(null);
+        if (updateDeliveryStatus != null){
+            updateDeliveryStatus.setDeliveryStatus(deliveryStatus);
+            deliveryRepo.save(updateDeliveryStatus);
+            }
+
+        }
+
+        public Optional<Delivery> getDeliveryById(Long deliveryId) {
+            return deliveryRepo.findById(deliveryId);
+        }
+
     }
 
-    public void updateDeliveryStatus(Long deliveryId,DeliveryStatus deliveryStatus) {
-           Delivery updateDelivery = deliveryRepo.findById(deliveryId).orElse(null);
-           updateDelivery.setDeliveryStatus(deliveryStatus);
-           deliveryRepo.save(updateDelivery);
-    }
-}
